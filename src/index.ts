b7336d01b8f47edbc86dad63c1d1e2184c5acbe4
Austin Gray <GrayAustinC@gmail.com>
2025-04-08 16:16:15 -0400

    fixed getting headers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import is from "./is";
 import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 
-function getClientIpFromXForwardedFor(value?: string): string | null {
+function getClientIpFromXForwardedFor(value?: string | null): string | null {
   if (!is.existy(value)) {
     return null;
   }
@@ -34,13 +34,13 @@ function getClientIpFromXForwardedFor(value?: string): string | null {
 
 function getClientIp(headers: ReadonlyHeaders): string | null {
   // Standard headers used by Amazon EC2, Heroku, and others.
-  if (is.ip(headers["x-client-ip"])) {
-    return headers["x-client-ip"];
+  if (is.ip(headers.get("x-client-ip"))) {
+    return headers.get("x-client-ip");
   }
 
   // Load-balancers (AWS ELB) or proxies.
   const xForwardedFor = getClientIpFromXForwardedFor(
-    headers["x-forwarded-for"]
+    headers.get("x-forwarded-for")
   );
   if (is.ip(xForwardedFor)) {
     return xForwardedFor;
@@ -49,59 +49,59 @@ function getClientIp(headers: ReadonlyHeaders): string | null {
   // Cloudflare.
   // @see https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-Cloudflare-handle-HTTP-Request-headers-
   // CF-Connecting-IP - applied to every request to the origin.
-  if (is.ip(headers["cf-connecting-ip"])) {
-    return headers["cf-connecting-ip"];
+  if (is.ip(headers.get("cf-connecting-ip"))) {
+    return headers.get("cf-connecting-ip");
   }
 
   // DigitalOcean.
   // @see https://www.digitalocean.com/community/questions/app-platform-client-ip
   // DO-Connecting-IP - applied to app platform servers behind a proxy.
-  if (is.ip(headers["do-connecting-ip"])) {
-    return headers["do-connecting-ip"];
+  if (is.ip(headers.get("do-connecting-ip"))) {
+    return headers.get("do-connecting-ip");
   }
 
   // Fastly and Firebase hosting header (When forwared to cloud function)
-  if (is.ip(headers["fastly-client-ip"])) {
-    return headers["fastly-client-ip"];
+  if (is.ip(headers.get("fastly-client-ip"))) {
+    return headers.get("fastly-client-ip");
   }
 
   // Akamai and Cloudflare: True-Client-IP.
-  if (is.ip(headers["true-client-ip"])) {
-    return headers["true-client-ip"];
+  if (is.ip(headers.get("true-client-ip"))) {
+    return headers.get("true-client-ip");
   }
 
   // Default nginx proxy/fcgi; alternative to x-forwarded-for, used by some proxies.
-  if (is.ip(headers["x-real-ip"])) {
-    return headers["x-real-ip"];
+  if (is.ip(headers.get("x-real-ip"))) {
+    return headers.get("x-real-ip");
   }
 
   // (Rackspace LB and Riverbed's Stingray)
   // http://www.rackspace.com/knowledge_center/article/controlling-access-to-linux-cloud-sites-based-on-the-client-ip-address
   // https://splash.riverbed.com/docs/DOC-1926
-  if (is.ip(headers["x-cluster-client-ip"])) {
-    return headers["x-cluster-client-ip"];
+  if (is.ip(headers.get("x-cluster-client-ip"))) {
+    return headers.get("x-cluster-client-ip");
   }
 
-  if (is.ip(headers["x-forwarded"])) {
-    return headers["x-forwarded"];
+  if (is.ip(headers.get("x-forwarded"))) {
+    return headers.get("x-forwarded");
   }
 
-  if (is.ip(headers["forwarded-for"])) {
-    return headers["forwarded-for"];
+  if (is.ip(headers.get("forwarded-for"))) {
+    return headers.get("forwarded-for");
   }
 
   // Google Cloud App Engine
   // https://cloud.google.com/appengine/docs/standard/go/reference/request-response-headers
 
-  if (is.ip(headers["x-appengine-user-ip"])) {
-    return headers["x-appengine-user-ip"];
+  if (is.ip(headers.get("x-appengine-user-ip"))) {
+    return headers.get("x-appengine-user-ip");
   }
 
   // Cloudflare fallback
   // https://blog.cloudflare.com/eliminating-the-last-reasons-to-not-enable-ipv6/#introducingpseudoipv4
   if (headers) {
-    if (is.ip(headers["Cf-Pseudo-IPv4"])) {
-      return headers["Cf-Pseudo-IPv4"];
+    if (is.ip(headers.get("Cf-Pseudo-IPv4"))) {
+      return headers.get("Cf-Pseudo-IPv4");
     }
   }
 
